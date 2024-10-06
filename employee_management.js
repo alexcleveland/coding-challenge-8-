657d2b427fe3511614cf8f0095417b31be764f8b
Alex Cleveland <acleveland2@usf.edu>
2024-10-05 20:07:17 -0400

    Create Manager class with inheritance from Employee

--- employee_management.js
+++ employee_management.js
@@ -25,3 +25,15 @@ class Department {
         return this.employees.reduce((total, emp) => total + emp.salary, 0);
     }
 }
+
+// Task 3 Create a Manager Class that Inherits from Employee
+
+class Manager extends Employee {
+    constructor(name, salary, position, department, bonus){
+        super(name, salary, position, department);
+        this.bonus = bonus;
+    }
+    getdetails(){
+        return`${this.name} - ${this.position}: $${this.salary} + Bonus: $${this.bonus}`;
+    }
+}
