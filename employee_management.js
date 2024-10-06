a1c99779a896bf74d1ffb09a6da98dc060985b29
Alex Cleveland <acleveland2@usf.edu>
2024-10-05 20:00:44 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -11,3 +11,17 @@ class Employee{
     }
 }
 
+// Task 2 Create a Department Class
+
+class Department {
+    constructor(name) {
+        this.name = name;
+        this.employees= [];
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    getDepartmentSalary() {
+        return this.employees.reduce((total, emp) => total + emp.salary, 0);
+    }
+}
